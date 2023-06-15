ae2df25a4ca0399c69beef4e87f3962a22b941de
Nathan Higley <contact@nathanhigley.com>
2023-06-15 17:29:30 -0500

    feat: enable terraform deployment of vyos

--- terraform/vyos.tf
+++ terraform/vyos.tf
@@ -1,16 +1,38 @@
-terraform {
-	backend "pg" {
-		schema_name = "vyos"
+resource "proxmox_vm_qemu" "vyos-core" {
+	name = "vyos-core"
+	desc = "VyOS Core Router"
+	qemu_os = "l26"
+	bios = "ovmf"
+	boot = "order=scsi0;ide2"
+	target_node = var.pm_node
+	onboot=true
+	startup="order=1"
+
+	sockets = 1
+	cores = 2
+	memory = 4096
+	agent = 1
+	clone = "vyos-1.4-rolling-cloudinit"
+	full_clone = "true"
+	scsihw = "virtio-scsi-single"
+
+	network {
+	 bridge    = "vmbr0"
+	 model     = "e1000"
 	}
-	required_providers {
-		proxmox = {
-			source = "Telmate/proxmox"
-			version = "2.9.14"
-		}
-	}
+	lifecycle {
+      ignore_changes = [
+        disk,
+      ]
+    }
+
+	os_type = "cloud-init"
+
+	cicustom                = "user=local:snippets/vyos-cloud-config.yml"
+	cloudinit_cdrom_storage = "local"
 }
 
-provider "proxmox" {
-	# Configuration options
-	pm_api_url = "https://proxmox.lab.astr0rack.net/api2/json"
+output "vyos_default_ip" {
+  value = proxmox_vm_qemu.vyos-core.default_ipv4_address
 }
+
