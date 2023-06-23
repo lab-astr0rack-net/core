4fbc820e1ff4f5a6dd0c65c30021d0e4ba3b8bb5
Nathan Higley <contact@nathanhigley.com>
2023-06-23 10:42:28 -0500

    feat: add github provider

--- terraform/keycloak/keycloak.tf
+++ terraform/keycloak/keycloak.tf
@@ -4,3 +4,16 @@ resource "keycloak_realm" "lab" {
   display_name      = "lab"
   display_name_html = "<b>lab</b>"
 }
+
+resource "keycloak_oidc_identity_provider" "github" {
+  realm = keycloak_realm.lab.realm
+  alias = "github"
+  provider_id = "github"
+  client_id = var.keycloak_github_client_id
+  client_secret = var.keycloak_github_client_secret
+  token_url = ""
+  authorization_url = ""
+  add_read_token_role_on_create = true
+  backchannel_supported = false
+  default_scopes = "read:user read:org user:email"
+}
