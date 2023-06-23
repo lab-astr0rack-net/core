ff995cf83c5a4ea9f0ebf96e1873a00e02c90b74
Nathan Higley <contact@nathanhigley.com>
2023-06-23 10:23:04 -0500

    feat: add keycloak and format

--- terraform/keycloak/main.tf
+++ terraform/keycloak/main.tf
@@ -1,23 +1,17 @@
 terraform {
   backend "pg" {}
   required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.14"
+    keycloak = {
+      source  = "mrparkers/keycloak"
+      version = "4.3.1"
     }
   }
 }
 
-provider "proxmox" {
+provider "keycloak" {
   # Configuration options
-  pm_api_url    = var.pm_api_url
-  pm_user       = var.pm_user
-  pm_password   = var.pm_password
-  pm_log_enable = true
-  pm_log_file   = "/tmp/terraform-plugin-proxmox.log"
-  pm_debug      = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
+  client_id = "admin-cli"
+  username  = var.keycloak_user
+  password  = var.keycloak_password
+  url       = var.keycloak_url
 }
