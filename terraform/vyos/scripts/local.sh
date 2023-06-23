27d21577dab3e2bfe3d9d606ac04ec846a092662
Nathan Higley <contact@nathanhigley.com>
2023-06-23 10:18:39 -0500

    feat: use matrix for terraform

---
+++ terraform/vyos/scripts/local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+if [ -z "$BW_SESSION" ]
+then
+	export BW_SESSION=$(bw unlock --raw)
+fi
+
+export PG_CONN_STR=$(bw get password terraform-proxmox-postgres)
+export PG_SCHEMA_NAME="vyos"
+export TF_VAR_pm_password=$(bw get password terraform-proxmox-credentials)
+
+terraform "$@"
