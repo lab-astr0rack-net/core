25ff90bafb04b6d2441872c85b103e2419d01da0
Nathan Higley <contact@nathanhigley.com>
2023-06-23 11:15:59 -0500

    feat: add necessary items to get keycloak to work with proxmox

--- terraform/keycloak/keycloak.tf
+++ terraform/keycloak/keycloak.tf
@@ -17,3 +17,34 @@ resource "keycloak_oidc_identity_provider" "github" {
   backchannel_supported         = false
   default_scopes                = "read:user read:org user:email"
 }
+
+resource "keycloak_openid_client" "proxmox" {
+  realm_id  = keycloak_realm.lab.id
+  client_id = "proxmox"
+
+  name    = "proxmox"
+  enabled = true
+
+  access_type                  = "CONFIDENTIAL"
+  client_secret                = var.keycloak_proxmox_client_secret
+  standard_flow_enabled        = true
+  implicit_flow_enabled        = true
+  direct_access_grants_enabeld = true
+  full_scope_allowed           = true
+
+  valid_redirect_uris = [
+    "https://proxmox.lab.astr0rack.net"
+  ]
+
+  login_theme = "keycloak"
+
+}
+
+resource "keycloak_openid_user_property_protocol_mapper" "proxmox_client_mapper" {
+  realm_id  = keycloak_realm.lab.id
+  client_id = keycloak_openid_client.proxmox.id
+  name      = "proxmox-client-mapper"
+
+  user_property = "username"
+  claim_name    = "username"
+}
